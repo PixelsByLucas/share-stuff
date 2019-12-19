7d4ef739cf70ad6f14c4f118ea46e1c312e47382
Lucas Knight <PixelsByLucas@users.noreply.github.com>
2019-12-19 16:24:47 -0500

    added karma & search input to Nav

--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -1,6 +1,18 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
+import KarmaIcon from "../assets/KarmaIcon";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+  icons: {
+    values: {
+      karma: {
+        component: KarmaIcon,
+        props: {
+          name: "karma"
+        }
+      }
+    }
+  }
+});
