8468c7c7fb4eb1f48f63ff1ac6b72c6f72fd9869
Lucas Knight <PixelsByLucas@users.noreply.github.com>
2019-12-31 13:50:30 -0500

    code cleanup

--- client/src/apis/items.js
+++ client/src/apis/items.js
@@ -17,24 +17,3 @@ export const createItem = (formData, token) => {
       console.log("ERROR: ", error);
     });
 };
-
-// export const registerRequest = ({
-//   email,
-//   password,
-//   fullName,
-//   username,
-//   location
-// }) => {
-//   const { firstName, lastName } = firstAndLastName(fullName);
-//   return axios({
-//     method: "post",
-//     url: `${SERVER_URL}/users`,
-//     data: { email, password, firstName, lastName, username, location }
-//   })
-//     .then(res => {
-//       return res.data;
-//     })
-//     .catch(error => {
-//       console.log("ERROR: ", error);
-//     });
-// };
