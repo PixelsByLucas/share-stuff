d334e3347bd116400fd04193a88292378397e9d2
Lucas Knight <PixelsByLucas@users.noreply.github.com>
2019-12-31 20:55:55 -0500

    UserBio component built

---
+++ client/src/components/UserBio.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="user-blurb">
+    <h1>{{ user.username }}</h1>
+    <h2>
+      <span>{{ name }}</span>
+      <span class="user-blurb__age">{{ "age: " + user.age }}</span>
+    </h2>
+    <p>{{ user.bio }}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: "UserBio",
+  props: ["user"],
+  computed: {
+    name() {
+      const first =
+        this.user.firstName.charAt(0).toUpperCase() +
+        this.user.firstName.slice(1);
+      const lastInitial = this.user.lastName.charAt(0).toUpperCase() + ".";
+      return `${first} ${lastInitial}`;
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+.user-blurb {
+  &__age {
+    margin-left: 1rem;
+  }
+}
+</style>
