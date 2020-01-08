fbf6e7c4fb2af8ace89dc167749ccef4aea45a40
Lucas Knight <PixelsByLucas@users.noreply.github.com>
2020-01-08 17:45:28 -0500

    Nav now saves search values to state

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -4,14 +4,16 @@ import Vuex from "vuex";
 import users from "./users";
 import items from "./items";
 import alerts from "./alerts";
+import search from "./search";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {},
   modules: {
-    users,
+    alerts,
     items,
-    alerts
+    search,
+    users
   }
 });
---
+++ client/src/store/search.js
@@ -0,0 +1,39 @@
+import { ITEM_CATEGORIES } from "../utils/constants";
+
+export default {
+  state: {
+    location: {
+      lat: "",
+      long: ""
+    },
+    searchTerm: "",
+    category: ""
+  },
+  mutations: {
+    SET_CATEGORY(state, payload) {
+      state.category = payload;
+    },
+    SET_LOCATION(state, payload) {
+      state.location = { lat: payload.lat, long: payload.long };
+    },
+    SET_TERM(state, payload) {
+      state.searchTerm = payload;
+    }
+  },
+  actions: {
+    setCategory({ commit }, payload) {
+      if (ITEM_CATEGORIES.includes(payload)) {
+        commit("SET_CATEGORY", payload);
+      } else {
+        // TODO: Commit notification here!
+        console.log("NOT APPROPRIATE CATEGORY");
+      }
+    },
+    setTerm({ commit }, payload) {
+      commit("SET_TERM", payload);
+    },
+    setLocation({ commit }, payload) {
+      commit("SET_LOCATION", payload);
+    }
+  }
+};
