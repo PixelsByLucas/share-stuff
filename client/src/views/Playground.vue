69c33bccefb441828b50d1a4e4e445d533e0c40b
Roman <gobkins@gmail.com>
2020-01-08 17:05:19 -0500

    initial commit

--- client/src/views/Playground.vue
+++ client/src/views/Playground.vue
@@ -1,19 +1,17 @@
 <template>
-  <!-- This vue can be used to test/build components in isolation from their context. -->
-  <!-- Navigate to the /playground route in the browser to see the component -->
-  <!-- Don't commit changes to this file -->
-  <div class="playground">
-    <h1>This is the playground page</h1>
-    <div>
-      <!-- Render the component you want to test here -->
-    </div>
+  <div class="home">
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
+
 <script>
-// Import the component you want to work with here.
-// Write all the dummy data the component needs to function here.
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
 export default {
-  name: "Playground"
+  name: "home",
+  components: {
+    HelloWorld
+  }
 };
 </script>
-<style lang="scss" scoped></style>
