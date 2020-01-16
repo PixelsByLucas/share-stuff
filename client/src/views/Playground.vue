1360d86dfc0d3412e9a488ff8b5a8bed523a2cb0
Lucas Knight <PixelsByLucas@users.noreply.github.com>
2020-01-15 21:20:31 -0500

    reset playgound

--- client/src/views/Playground.vue
+++ client/src/views/Playground.vue
@@ -2,28 +2,18 @@
   <!-- This vue can be used to test/build components in isolation from their context. -->
   <!-- Navigate to the /playground route in the browser to see the component -->
   <!-- Don't commit changes to this file -->
-  <!-- <div class="playground">
+  <div class="playground">
     <h1>This is the playground page</h1>
-  <div>-->
-  <Map v-on:update-center="test" />
-  <!-- Render the component you want to test here -->
-  <!-- </div>
-  </div>-->
+    <div>
+      <!-- Render the component you want to test here -->
+    </div>
+  </div>
 </template>
 <script>
-import Map from "../components/Map";
 // Import the component you want to work with here.
 // Write all the dummy data the component needs to function here.
 export default {
-  name: "Playground",
-  components: {
-    Map
-  },
-  methods: {
-    test(coords) {
-      console.log(coords);
-    }
-  }
+  name: "Playground"
 };
 </script>
 <style lang="scss" scoped></style>
